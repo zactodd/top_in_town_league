9bb1c0f5526d6ec7967c6d9ce11bfba6de85dbdb
Zac Todd <zactodd0@gmail.com>
2021-06-18 11:28:20 +1200

    Update visualise.py

--- visualise.py
+++ visualise.py
@@ -1,30 +1,47 @@
 from utils import update_rankings_from_match
+from collections import defaultdict
 
 
 def pprint_rankings_history(previous_ranking_info, matches_info):
     order_players = sorted(previous_ranking_info.keys())
     rankings = previous_ranking_info.copy()
+    other_metrics = defaultdict(lambda: {"scores": [], "wins": 0, "played": 0})
     num_players = len(previous_ranking_info)
     
     format_row = " {:<15}|" * (num_players + 1)
     score_row = " {:<15}|" + " {:>15}|" * num_players
-    line_row = "-" * len(format_row.format(*tuple([""] * (num_players + 1))))
+    line = "-" * len(format_row.format(*tuple([""] * (num_players + 1))))
+
+    double_line = f"{line}\n{line}"
 
     print(format_row.format("players", *sorted(previous_ranking_info.keys())))
+    print(line)
     print(score_row.format("mu", *(f'{previous_ranking_info[p].mu:.2f}' for p in order_players)))
-    print(line_row)
-    print(line_row)
+    print(double_line)
 
     for game_idx, m in enumerate(matches_info, 1):
         teams = {}
-        score = {}
+        scores = {}
         for i, t in enumerate(m, 1):
-            teams.update({p: "-".join(t["team"]) for p in t["team"]})
-            score.update({p: t["score"] for p in t["team"]})
+            for p in t["team"]:
+                score = t["score"]
+                teams[p] = "-".join(t["team"])
+                scores[p] = score
+                other_metrics[p]["scores"].append(score)
+                other_metrics[p]["played"] += 1
+                if score >= 10:
+                    other_metrics[p]["wins"] += 1
 
         rankings.update(update_rankings_from_match(rankings, m))
-
         print(format_row.format("team", *(teams[p] if p in teams else "-" for p in order_players)))
-        print(score_row.format("score", *(score[p] if p in score else "-" for p in order_players)))
+        print(score_row.format("score", *(scores[p] if p in scores else "-" for p in order_players)))
+        print(line)
         print(score_row.format("mu", *(f'{rankings[p].mu:.2f}' for p in order_players)))
-        print(line_row)
+        print(score_row.format("wins", *(other_metrics[p]["wins"] if p in other_metrics else "-" for p in order_players)))
+        print(score_row.format("win rate",
+                               *(f'{other_metrics[p]["wins"] / other_metrics[p]["played"]:.2f}'
+                                 if p in other_metrics else "-" for p in order_players)))
+        print(score_row.format("total score", *(sum(other_metrics[p]["scores"]) if p in other_metrics else "-" for p in order_players)))
+        print(score_row.format("avg score", *(f'{sum(other_metrics[p]["scores"]) / other_metrics[p]["played"]:.2f}' if p in other_metrics else "-" for p in order_players)))
+        print(double_line)
+
