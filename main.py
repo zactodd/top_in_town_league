f358b81327199c188dc9b65d7bd5d060b956e81a
Zac Todd <zactodd0@gmail.com>
2021-06-24 21:15:56 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import utils
 import visualise
 import json
-
+import pprint
 
 with open(utils.HISTORY_PATH) as f:
     games_info = json.load(f)
 
-rank_info = utils.load_init_rankings(games_info["starting_info"])
-visualise.pprint_rankings_history(rank_info, games_info["matches"])
+
+FILE = "/home/tutor/Downloads/Unofficial Official Top In Town League - Sheet1.csv"
+rank_info, matches_info = utils.csv_to_game_record(FILE)
+pprint.pprint(matches_info)
+visualise.pprint_rankings_history(rank_info, matches_info)
+
