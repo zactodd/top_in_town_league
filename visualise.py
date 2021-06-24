ea3a01d92d3df67e41e2090f5e4c3fbb44ff157c
Zac Todd <zactodd0@gmail.com>
2021-06-24 21:15:35 +1200

    Update visualise.py

--- visualise.py
+++ visualise.py
@@ -1,13 +1,15 @@
 from utils import update_rankings_from_match
 from collections import defaultdict
+from matplotlib import pyplot as plt
+from itertools import accumulate
 
 
 def pprint_rankings_history(previous_ranking_info, matches_info):
     order_players = sorted(previous_ranking_info.keys())
+
     rankings = previous_ranking_info.copy()
     other_metrics = defaultdict(lambda: {"scores": [], "wins": 0, "played": 0})
     num_players = len(previous_ranking_info)
-    
     format_row = " {:<15}|" * (num_players + 1)
     score_row = " {:<15}|" + " {:>15}|" * num_players
     line = "-" * len(format_row.format(*tuple([""] * (num_players + 1))))
@@ -31,36 +33,37 @@ def pprint_rankings_history(previous_ranking_info, matches_info):
                 other_metrics[p]["played"] += 1
                 if score >= 10:
                     other_metrics[p]["wins"] += 1
-
         rankings.update(update_rankings_from_match(rankings, m))
         game_prints = pteam, pscore = [], []
         post_prints = pmu, pwins, pwinrate, ptotal, pavg = [], [], [], [], []
-        without_init = pteam, pscore, pwins, pwinrate, ptotal, pavg
         for p in order_players:
             pmu.append(f'{rankings[p].mu:.2f}')
-            if p in scores:
+            if p in teams:
                 pteam.append(teams[p])
                 pscore.append(scores[p])
             else:
-                for i in game_prints:
-                    i.append("-")
+                pteam.append("-")
+                pscore.append("-")
             if p in other_metrics:
                 pwins.append(other_metrics[p]["wins"])
                 pwinrate.append(f'{other_metrics[p]["wins"] / other_metrics[p]["played"]:.2f}')
                 ptotal.append(sum(other_metrics[p]["scores"]))
                 pavg.append(f'{sum(other_metrics[p]["scores"]) / other_metrics[p]["played"]:.2f}')
             else:
-                for i in without_init:
-                    i.append("-")
+                pwins.append("-")
+                pwinrate.append("-")
+                ptotal.append("-")
+                pavg.append("-")
         print("\n".join(score_row.format(n, *s) for n, s in zip(("team", "score"), game_prints)))
         print(line)
         print("\n".join(score_row.format(n, *s)
                         for n, s in zip(("mu", "wins", "win rate", "total score", "avg score"), post_prints)))
         print(double_line)
 
-    prints = pmu, pwins, pwinrate, ptotal, pavg = [], [], [], [], []
+    prints = pmu, pplayed, pwins, pwinrate, ptotal, pavg = [], [], [], [], [], []
     for p in order_players:
         pmu.append(f'{rankings[p].mu:.2f}')
+        pplayed.append(other_metrics[p]["played"])
         pwins.append(other_metrics[p]["wins"])
         pwinrate.append(f'{other_metrics[p]["wins"] / other_metrics[p]["played"]:.2f}')
         ptotal.append(sum(other_metrics[p]["scores"]))
@@ -70,9 +73,57 @@ def pprint_rankings_history(previous_ranking_info, matches_info):
     print(format_row.format("players", *sorted(previous_ranking_info.keys())))
     print(line)
     print("\n".join(score_row.format(n, *s)
-                    for n, s in zip(("mu", "wins", "win rate", "total score", "avg score"), prints)))
+                    for n, s in zip(("mu", "played", "wins", "win rate", "total score", "avg score"), prints)))
     print(line)
 
 
+def plot_mu_over_games(previous_ranking_info, matches_info):
+    rankings = previous_ranking_info.copy()
+    mu_per_game = {p: [r.mu] for p, r in rankings.items()}
+
+    for game_idx, m in enumerate(matches_info, 1):
+        rankings.update(update_rankings_from_match(rankings, m))
+        for p, r in rankings.items():
+            mu_per_game[p].append(r.mu)
+
+    for x in mu_per_game.values():
+        plt.plot(range(1, len(x) + 1), x)
+    plt.show()
 
 
+def plot_delta_mu_over_games(previous_ranking_info, matches_info):
+    rankings = previous_ranking_info.copy()
+    mu_per_game = {p: [r.mu] for p, r in rankings.items()}
+
+    for game_idx, m in enumerate(matches_info, 1):
+        rankings.update(update_rankings_from_match(rankings, m))
+        for p, r in rankings.items():
+            mu_per_game[p].append(r.mu)
+
+    delta_mus = {p: [j - i for i, j in zip(mu[:-1], mu[1:])] for p, mu in mu_per_game.items()}
+    for x in delta_mus.values():
+        plt.scatter(range(1, len(x) + 1), x)
+    plt.show()
+
+
+def plot_avg_score_over_games(previous_ranking_info, matches_info):
+    order_players = sorted(previous_ranking_info.keys())
+    scores = defaultdict(list)
+
+    for m in matches_info:
+        played = set()
+        for t in m:
+            for p in t["team"]:
+                scores[p].append(t["score"])
+                played.add(p)
+        for p in order_players:
+            if p not in played:
+                scores[p].append(None)
+    for s in scores.values():
+        x = [avg for avg, *_ in
+             accumulate(s,
+                        lambda t, a: t if a is None else ((t[1] + a) / (t[2] + 1), t[1] + a, t[2] + 1),
+                        initial=(0, 0, 0))
+             ]
+        plt.scatter(range(1, len(x) + 1), x)
+    plt.show()
