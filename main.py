3c6f30983983657656c1fbbd6060228a6acab767
Zac Todd <zactodd0@gmail.com>
2021-06-24 21:35:05 +1200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,4 @@ import pprint
 
 FILE = "/home/tutor/Downloads/Unofficial Official Top In Town League - Sheet1.csv"
 rank_info, matches_info = utils.csv_to_game_record(FILE)
-pprint.pprint(matches_info)
 visualise.pprint_rankings_history(rank_info, matches_info)
-
