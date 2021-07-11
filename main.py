ec4507a026a6d7b773bbd8a7adeafa444f834242
Zac Todd <zactodd0@gmail.com>
2021-07-11 17:49:43 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 from PIL import Image
 import cv2
-from collections import Counter
 import utils
 import visualise
 import os
@@ -9,13 +8,7 @@ import os
 
 FILE = "Unofficial Official Top In Town League - Sheet1.csv"
 rank_info, matches_info = utils.csv_to_game_record(FILE)
-
-played = Counter()
-for m in matches_info:
-    for t in m:
-        for p in t["team"]:
-            played[p] += 1
-players = sorted({k for k, v in played.items() if v >= 5})
+players = sorted(utils.players_with_min_matches(matches_info, 5))
 
 visualise.pprint_rankings_history(rank_info, matches_info)
 
@@ -33,3 +26,4 @@ for p in players:
 
 img, *imgs = [Image.open(f"figs/{p}_combined.png") for p in players]
 img.save(fp="stats.gif", format='GIF', append_images=imgs, save_all=True, duration=2500, loop=0)
+visualise.combinations_wins_distribution(matches_info)
